8a58168e9581ab1964d721de6ec0fc13baf12a7d
bhavyamohan <bhavyamohan9@gmail.com>
2016-11-12 00:34:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', '9.html'));
+  res.sendFile(path.join(__dirname, '9.html'));
 });
 
 app.get('/articletwo', function (req, res) {
